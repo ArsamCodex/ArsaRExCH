34f729c2619ad7baac6a2180bd7a896d0d376663
ArsaMUK <ArsaM@DESKTOP-DT26U4I>
2024-05-18 10:22:30 +0100

    pair contorller

---
+++ ArsaRExCH/PairController.cs
@@ -0,0 +1,34 @@
+﻿using ArsaRExCH.Data;
+using ArsaRExCH.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArsaRExCH
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PairController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public PairController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPairs(Pair pair)
+        {
+            try
+            {
+                await _context.Pair.AddAsync(pair);
+                await _context.SaveChangesAsync();
+                return Ok(pair);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        } 
+    }
+}
