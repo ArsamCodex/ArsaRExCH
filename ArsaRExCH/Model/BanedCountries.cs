17d0d823c270edfbbcce74965a90a48f1b3d4cf6
ArsaMUK <ArsaM@DESKTOP-DT26U4I>
2024-09-21 20:22:51 +0100

    update banned countries class

--- ArsaRExCH/Model/BanedCountries.cs
+++ ArsaRExCH/Model/BanedCountries.cs
@@ -8,7 +8,7 @@
  * you go to this site to see ip range of country https://lite.ip2location.com/ip-address-ranges-by-country?lang=en_US
  * for example uk ip 62.102.95.0	  public ( 1 of ) here you put onlly 62 in database its unique range belongs onlly
  * to uk , or letme saay first digits from left to . u ut the digits to database and NOT whole Ip
- * program no banns first digit i will improve it to first 3 digit 
+ * program now calculates first 2 digit i will improve it to first 3 digit 
  * 
  * 
  */ 
