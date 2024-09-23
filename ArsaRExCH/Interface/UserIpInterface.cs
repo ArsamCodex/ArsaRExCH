c807806f82d10ceb79ae325e3da0b92f8cbbc908
ArsaMUK <ArsaM@DESKTOP-DT26U4I>
2024-09-23 22:56:42 +0100

    Adminpanel empty added

--- ArsaRExCH/Interface/UserIpInterface.cs
+++ ArsaRExCH/Interface/UserIpInterface.cs
@@ -7,6 +7,10 @@ namespace ArsaRExCH.Interface
         Task<string> GetPublicIpAddress();
         Task<string> CheckIfIpIsBanned(string userIpAddress);
         string ExtractFirstPart(string ipAddress);
+        /*This method returns list of maxinzetdto 
+         * of most inzet u user whi price most inzet are and total 
+         * bet
+         */
         Task<List<MaxInzetResultDTO>> CalculateMaxInzetAsync();
     }
 }
