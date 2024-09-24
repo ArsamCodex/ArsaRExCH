d3892430f71839ed76ecc2f09a90d56a00a4eca9
ArsaMUK <ArsaM@DESKTOP-DT26U4I>
2024-09-24 08:24:53 +0100

    Administration Iterface /impl now witout impl

---
+++ ArsaRExCH/Interface/AdministrationInterface.cs
@@ -0,0 +1,13 @@
+﻿using ArsaRExCH.Model;
+
+namespace ArsaRExCH.Interface
+{
+    public interface AdministrationInterface
+    {
+        Task<BanedCountries> AddBannCountries();
+        Task<BanedCountries> GetAllBannedCountriesInDatabase();
+        Task<BanedCountries> RemoveBannedCuntries();
+        Task<BanedCountries> EditBannedCountries();
+        
+    }
+}
---
+++ ArsaRExCH/InterfaceIMPL/AdministrationInterfaceIMPL.cs
@@ -0,0 +1,28 @@
+﻿using ArsaRExCH.Interface;
+using ArsaRExCH.Model;
+
+namespace ArsaRExCH.InterfaceIMPL
+{
+    public class AdministrationInterfaceIMPL : AdministrationInterface
+    {
+        public Task<BanedCountries> AddBannCountries()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BanedCountries> EditBannedCountries()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BanedCountries> GetAllBannedCountriesInDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BanedCountries> RemoveBannedCuntries()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
