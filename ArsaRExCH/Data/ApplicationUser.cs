9a6ee67fe3e49963cf6a51391186a5a576dcb1d0
ArsaMUK <ArsaM@DESKTOP-DT26U4I>
2024-09-28 14:23:32 +0100

    AirDrop sample class

--- ArsaRExCH/Data/ApplicationUser.cs
+++ ArsaRExCH/Data/ApplicationUser.cs
@@ -10,6 +10,7 @@ namespace ArsaRExCH.Data
         // public ICollection<Wallet> Wallets { get; set; }
         public DateTime? LastLoginDate { get; set; }
         public List<UserDatesRecord> UserLoginDates { get; set; } = new List<UserDatesRecord>();
+        public List<AirDrop> UserAirdops { get; set; } = new List<AirDrop>();
 
     }
 
---
+++ ArsaRExCH/Model/AirDrop.cs
@@ -0,0 +1,16 @@
+﻿using ArsaRExCH.Data;
+
+namespace ArsaRExCH.Model
+{
+    public class AirDrop
+    {
+        public int AirDropID { get; set; }
+        public int AirDropBalance { get; set; }
+        public DateTime TimeClick { get; set; }
+        // Foreign key property for ApplicationUser
+        public string ApplicationUserId { get; set; }
+
+        // Navigation property
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
