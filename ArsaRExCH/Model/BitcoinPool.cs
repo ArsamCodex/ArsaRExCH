248da771bd0ab2a7f7b97503b8e7c057191b3e2e
ArsaMUK <ArsaM@DESKTOP-DT26U4I>
2024-10-05 11:10:47 +0100

    Add Some property poolclass

--- ArsaRExCH/Model/BitcoinPool.cs
+++ ArsaRExCH/Model/BitcoinPool.cs
@@ -5,6 +5,13 @@
         public int BitcoinPoolId { get; set; }
         public string PoolName { get; set; }
         public double PoolCurrentBalance { get; set; }
+        public bool IsPoolActive { get; set; }
+        /* If IsPoolActive is false then SuspendDate gets initialized*/
+        public DateTime? SuspendDate { get; set; }
+        /*Date when a pool get rich by admin*/
         public DateTime Daterefilled { get; set; }
+        /*track adminID*/
+        public string AdminUserId { get; set; }
+        public DateTime? PoolOpenedDate { get; set; }
     }
 }
