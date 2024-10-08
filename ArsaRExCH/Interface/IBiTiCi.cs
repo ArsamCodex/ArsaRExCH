08bfdd6457c0f2abb9fd3a3fa5a3dd655613cd71
ArsaMUK <ArsaM@DESKTOP-DT26U4I>
2024-10-09 00:02:49 +0100

    comment

--- ArsaRExCH/Interface/IBiTiCi.cs
+++ ArsaRExCH/Interface/IBiTiCi.cs
@@ -2,6 +2,10 @@
 {
     public interface IBiTiCi
     {
+        /* To implement this method i need Bitcoin Nodeto on my machine make transactions at the time i dont have 
+         * any pc to run Node , instead i can impelement client for all exchanges like Binance okex bybit to make transactions from there 
+         * for get this done we need to make client and api calls , 
+         */
         Task<string> WithdrawBtcAsync(string privateKey, string recipientAddress, decimal amount);
 
     }
