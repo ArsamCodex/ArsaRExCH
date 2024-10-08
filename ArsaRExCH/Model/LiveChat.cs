41fe5229f06169b8dee5d4b3c20b35ea20fdeeed
ArsaMUK <ArsaM@DESKTOP-DT26U4I>
2024-10-08 07:28:25 +0100

    some changes

--- ArsaRExCH/Model/LiveChat.cs
+++ ArsaRExCH/Model/LiveChat.cs
@@ -4,6 +4,8 @@ namespace ArsaRExCH.Model
 {
     public class LiveChat
     {
+        /*Here for live chat you can use SignalR for real time but i like less librries in my app
+         * thats why i implement manually you can use if you like */
         public int LiveChatId { get; set; }
         public string  Message { get; set; }
         public DateTime TimeAndDate { get; set; }
