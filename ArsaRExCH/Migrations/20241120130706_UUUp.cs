660bceddf93208718725620ae530bc734076700d
ArsaMUK <ArsaM@DESKTOP-DT26U4I>
2024-11-20 18:46:55 +0000

    improve

---
+++ ArsaRExCH/Migrations/20241120130706_UUUp.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArsaRExCH.Migrations
+{
+    /// <inheritdoc />
+    public partial class UUUp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "bca9892e-61c8-45da-958d-ddad1f38a609", "d00d2b81-e9d2-419c-8765-7e64aaa07dc5" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "bca9892e-61c8-45da-958d-ddad1f38a609");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "d00d2b81-e9d2-419c-8765-7e64aaa07dc5");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "0616490e-7f72-4a95-9959-39c2ce013dc0", null, "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LastLoginDate", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "4a61cf1e-f4c6-4433-b0de-250542e2e702", 0, "5d7900a7-c699-4dc0-9ae1-84f47599bd89", "ARMINTTWAT@GMAIL.COM", true, null, false, null, "ARMINTTWAT@GMAIL.COM", "ARMINTTWAT@GMAIL.COM", "AQAAAAIAAYagAAAAEDUnZz/KjYxPuCxkRvVnTE9MIXt6Ffoo5LdJhV9qI7q2vqDUHQ6tBVrxE5+G+eYqPA==", null, false, "b95dfbb9-f57e-4a0f-9695-c716b7091e33", false, "arminttwat@gmail.com" });
+
+            migrationBuilder.UpdateData(
+                table: "Pair",
+                keyColumn: "PairID",
+                keyValue: 1,
+                column: "ListedDate",
+                value: new DateTime(2024, 11, 20, 13, 7, 4, 984, DateTimeKind.Local).AddTicks(9284));
+
+            migrationBuilder.UpdateData(
+                table: "Pair",
+                keyColumn: "PairID",
+                keyValue: 2,
+                column: "ListedDate",
+                value: new DateTime(2024, 11, 20, 13, 7, 4, 984, DateTimeKind.Local).AddTicks(9330));
+
+            migrationBuilder.UpdateData(
+                table: "Pair",
+                keyColumn: "PairID",
+                keyValue: 3,
+                column: "ListedDate",
+                value: new DateTime(2024, 11, 20, 13, 7, 4, 984, DateTimeKind.Local).AddTicks(9333));
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "0616490e-7f72-4a95-9959-39c2ce013dc0", "4a61cf1e-f4c6-4433-b0de-250542e2e702" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "0616490e-7f72-4a95-9959-39c2ce013dc0", "4a61cf1e-f4c6-4433-b0de-250542e2e702" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0616490e-7f72-4a95-9959-39c2ce013dc0");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "4a61cf1e-f4c6-4433-b0de-250542e2e702");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "bca9892e-61c8-45da-958d-ddad1f38a609", null, "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LastLoginDate", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "d00d2b81-e9d2-419c-8765-7e64aaa07dc5", 0, "0f3931cc-0ab7-4e39-832d-38de9a6c5515", "ARMINTTWAT@GMAIL.COM", true, null, false, null, "ARMINTTWAT@GMAIL.COM", "ARMINTTWAT@GMAIL.COM", "AQAAAAIAAYagAAAAEDUnZz/KjYxPuCxkRvVnTE9MIXt6Ffoo5LdJhV9qI7q2vqDUHQ6tBVrxE5+G+eYqPA==", null, false, "39b825a0-606f-4df9-8b35-a59e21d28be4", false, "arminttwat@gmail.com" });
+
+            migrationBuilder.UpdateData(
+                table: "Pair",
+                keyColumn: "PairID",
+                keyValue: 1,
+                column: "ListedDate",
+                value: new DateTime(2024, 11, 20, 1, 27, 43, 560, DateTimeKind.Local).AddTicks(8602));
+
+            migrationBuilder.UpdateData(
+                table: "Pair",
+                keyColumn: "PairID",
+                keyValue: 2,
+                column: "ListedDate",
+                value: new DateTime(2024, 11, 20, 1, 27, 43, 560, DateTimeKind.Local).AddTicks(8652));
+
+            migrationBuilder.UpdateData(
+                table: "Pair",
+                keyColumn: "PairID",
+                keyValue: 3,
+                column: "ListedDate",
+                value: new DateTime(2024, 11, 20, 1, 27, 43, 560, DateTimeKind.Local).AddTicks(8655));
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "bca9892e-61c8-45da-958d-ddad1f38a609", "d00d2b81-e9d2-419c-8765-7e64aaa07dc5" });
+        }
+    }
+}
