79cd13a754684aeac008efaafad483f56c8d5a2f
ArsaMUK <ArsaM@DESKTOP-DT26U4I>
2024-11-20 12:14:23 +0000

    Coupon

--- ArsaRExCH/Model/Coupon.cs
+++ ArsaRExCH/Model/Coupon.cs
@@ -1,21 +1,26 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace ArsaRExCH.Model
 {
     public class Coupon
     {
         public string CouponId { get; set; }
+        [Required]
         public string Details { get; set; }
+        [Required]
         public DateTime DateIssued { get; set; }
         public DateTime DateReedemt { get; set; }
+        [Required]
         public string Receiver { get; set; }
+        [Required]
         [Precision(18,8)]
         public decimal Amount { get; set; }
         public bool IsUsed { get; set; }
         public bool IsExpired { get; set; }
         public DateTime? ExpirationDate { get; set; }
+        [Required]
         public string IssuedByAdmin { get; set; }
-
 
 
     }
